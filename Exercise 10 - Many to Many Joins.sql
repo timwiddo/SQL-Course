9d662584b15254deb95eab853e7741be98303313
Tim Widmoser <82330733+timwiddo@users.noreply.github.com>
2024-06-22 11:02:34 +0200

    Add files via upload

---
+++ Exercise 10 - Many to Many Joins.sql
@@ -0,0 +1,121 @@
+CREATE TABLE reviewers (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL
+    );
+    
+CREATE TABLE series (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(100),
+	released_year YEAR,
+    genre VARCHAR(100)
+);
+
+CREATE TABLE reviews (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    rating DECIMAL(2, 1),
+	series_id INT,
+    reviewer_id INT,
+    FOREIGN KEY (series_id) REFERENCES series(id),
+    FOREIGN KEY (reviewer_id) REFERENCES reviewers(id)
+);
+
+INSERT INTO series (title, released_year, genre) VALUES
+    ('Archer', 2009, 'Animation'),
+    ('Arrested Development', 2003, 'Comedy'),
+    ("Bob's Burgers", 2011, 'Animation'),
+    ('Bojack Horseman', 2014, 'Animation'),
+    ("Breaking Bad", 2008, 'Drama'),
+    ('Curb Your Enthusiasm', 2000, 'Comedy'),
+    ("Fargo", 2014, 'Drama'),
+    ('Freaks and Geeks', 1999, 'Comedy'),
+    ('General Hospital', 1963, 'Drama'),
+    ('Halt and Catch Fire', 2014, 'Drama'),
+    ('Malcolm In The Middle', 2000, 'Comedy'),
+    ('Pushing Daisies', 2007, 'Comedy'),
+    ('Seinfeld', 1989, 'Comedy'),
+    ('Stranger Things', 2016, 'Drama');
+    
+ INSERT INTO reviewers (first_name, last_name) VALUES
+    ('Thomas', 'Stoneman'),
+    ('Wyatt', 'Skaggs'),
+    ('Kimbra', 'Masters'),
+    ('Domingo', 'Cortes'),
+    ('Colt', 'Steele'),
+    ('Pinkie', 'Petit'),
+    ('Marlon', 'Crafford');   
+    
+ INSERT INTO reviews(series_id, reviewer_id, rating) VALUES
+    (1,1,8.0),(1,2,7.5),(1,3,8.5),(1,4,7.7),(1,5,8.9),
+    (2,1,8.1),(2,4,6.0),(2,3,8.0),(2,6,8.4),(2,5,9.9),
+    (3,1,7.0),(3,6,7.5),(3,4,8.0),(3,3,7.1),(3,5,8.0),
+    (4,1,7.5),(4,3,7.8),(4,4,8.3),(4,2,7.6),(4,5,8.5),
+    (5,1,9.5),(5,3,9.0),(5,4,9.1),(5,2,9.3),(5,5,9.9),
+    (6,2,6.5),(6,3,7.8),(6,4,8.8),(6,2,8.4),(6,5,9.1),
+    (7,2,9.1),(7,5,9.7),
+    (8,4,8.5),(8,2,7.8),(8,6,8.8),(8,5,9.3),
+    (9,2,5.5),(9,3,6.8),(9,4,5.8),(9,6,4.3),(9,5,4.5),
+    (10,5,9.9),
+    (13,3,8.0),(13,4,7.2),
+    (14,2,8.5),(14,3,8.9),(14,4,8.9);   
+    
+/*-------------------------------------------*/
+-- Challenge #1 
+    
+SELECT title, rating
+FROM series s JOIN reviews r ON s.id = r.series_id;
+    
+/*-------------------------------------------*/
+ -- Challenge #2 - Title and AVG rating
+ 
+SELECT title, AVG(rating) AS average_rating
+FROM series s JOIN reviews r ON s.id = r.series_id
+GROUP BY title
+ORDER BY average_rating;   
+    
+/*-------------------------------------------*/
+ -- Challenge #3 - match reviewers to their reviews    
+    
+SELECT first_name, last_name, rating
+FROM reviewers rs
+JOIN reviews r ON r.reviewer_id = rs.id;   
+    
+/*-------------------------------------------*/
+ -- Challenge #4 - identify series without reviews 
+    
+SELECT title AS unreviewed
+FROM series s
+LEFT JOIN reviews r ON r.series_id = s.id -- until here, it returns every series with a review so we need a left join
+ WHERE rating IS NULL;    
+    
+/*-------------------------------------------*/
+ -- Challenge #5 - Get each genres avg rating 
+    
+SELECT genre, ROUND(AVG(rating), 2) AS average_rating
+FROM series s JOIN reviews r ON r.series_id = s.id
+GROUP BY genre;
+    
+/*-------------------------------------------*/
+ -- Challenge #6 - Get each reviewers f/l - name plus review count and min max scores they gave.Set their status to ACTIVE if they have left at least 2 reviews 
+
+SELECT first_name, 
+				 last_name, 
+                 COUNT(*) AS review_count, 
+                 MIN(rating) AS min_rating, MAX(rating) AS max_rating,
+                 CASE
+					WHEN COUNT(*) > 1 THEN 'ACTIVE'
+                    ELSE 'INACTIVE'
+                 END AS status
+FROM reviewers rw
+LEFT JOIN reviews r ON rw.id = r.reviewer_id
+GROUP BY first_name, last_name;    
+    
+/*-------------------------------------------*/
+ -- Challenge #7 - Get title rating and reviewer (2 JOINS involved)
+
+SELECT title, rating, CONCAT(first_name, ' ', last_name) AS reviewer
+FROM series s JOIN reviews r ON r.series_id = s.id   
+JOIN reviewers rw ON rw.id = r.reviewer_id;  
+    
+/*-------------------------------------------*/
+    
