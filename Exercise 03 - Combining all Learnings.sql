a26a830273f890ded868b3b1da1c550f170bf94b
Tim Widmoser <82330733+timwiddo@users.noreply.github.com>
2024-06-26 17:19:43 +0200

    Update Exercise 03 - Combining all Learnings.sql

--- Exercise 03 - Combining all Learnings.sql
+++ Exercise 03 - Combining all Learnings.sql
@@ -7,7 +7,7 @@ CREATE DATABASE shirts_db ;
 -- Create Table shirts
 
 CREATE TABLE shirts (
-	shirt_id INT AUTO_INCREMENT,
+    shirt_id INT AUTO_INCREMENT,
     article VARCHAR(50) ,
     color VARCHAR(50) ,
     shirt_size VARCHAR(5) ,
@@ -19,14 +19,14 @@ CREATE TABLE shirts (
     
     INSERT INTO shirts (article, color, shirt_size, last_worn)
     VALUES 
-('t-shirt', 'white', 'S', 10),
-('t-shirt', 'green', 'S', 200),
-('polo shirt', 'black', 'M', 10),
-('tank top', 'blue', 'S', 50),
-('t-shirt', 'pink', 'S', 0),
-('polo shirt', 'red', 'M', 5),
-('tank top', 'white', 'S', 200),
-('tank top', 'blue', 'M', 15);
+	('t-shirt', 'white', 'S', 10),
+	('t-shirt', 'green', 'S', 200),
+	('polo shirt', 'black', 'M', 10),
+	('tank top', 'blue', 'S', 50),
+	('t-shirt', 'pink', 'S', 0),
+	('polo shirt', 'red', 'M', 5),
+	('tank top', 'white', 'S', 200),
+	('tank top', 'blue', 'M', 15);
 
 -- Get all the Data
 
